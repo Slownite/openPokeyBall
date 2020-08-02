afcbd39e1927410ad99251624b4a4349ef3e5095
Samuel Diop <snfdiop@outlook.om>
2020-08-02 12:53:03 -0700

    heroku setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT | 3000 
+app.use(static('./public/js'))
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+app.listen(PORT, ()=>{
+    console.log("you are in the server")
+})
