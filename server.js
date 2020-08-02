61cca5b9645c9ddf4dd951b909399773f58cae01
Samuel Diop <snfdiop@outlook.om>
2020-08-02 13:04:12 -0700

    correction static file serving

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT | 3000 
-app.use(express.static('./public/js'))
+app.use(express.static('./public/'))
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
