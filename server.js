32ca342203133f3392d1d49b6b2936c99db2c4f5
Samuel Diop <snfdiop@outlook.om>
2020-08-02 12:56:43 -0700

    heroku correction

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const path = require('path')
-const path = require('path')
 const app = express()
 const PORT = process.env.PORT | 3000 
-app.use(static('./public/js'))
+app.use(express.static('./public/js'))
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
